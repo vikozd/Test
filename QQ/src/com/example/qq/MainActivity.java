cb95b00c1f9fcb0b6457deb4617adc629a47fd55
viko <532870708@qq.com>
2015-07-09 11:30:32 +0800

    qq

---
+++ QQ/src/com/example/qq/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.qq;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		System.out.println("QQQQ");
+	}
+
+public void dasd(){
+	System.out.println("sadfd");
+}
+	
+}
